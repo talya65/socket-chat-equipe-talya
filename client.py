c33f142a3162aea3e2d69a073e039ba37986be70
Talya <talyas491@gmail.com>
2025-12-02 22:28:18 -0300

    mudança para mensagens privadas

--- client.py
+++ client.py
@@ -2,63 +2,50 @@
 import threading
 import socket
 
-
 def main():
-  # Cria um objeto de soquete para o cliente
-  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Cria um objeto de soquete para o cliente
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    try:
+        # Conecta ao servidor
+        client.connect(('localhost', 7777))
+    except:
+        return print('\nNão foi possível se conectar ao servidor!\n')
 
-  try:
-      # Tenta se conectar ao servidor na porta 7777
-      client.connect(('localhost', 7777))
-  except:
-      # Se não conseguir se conectar, exibe uma mensagem e encerra o programa
-      return print('\nNão foi possível se conectar ao servidor!\n')
+    # Solicita nome de usuário
+    username = input('Usuário> ')
+    # Envia nome de usuário ao servidor
+    client.send(username.encode('utf-8'))
+    print('\nConectado! Use /w <usuario> <mensagem> para enviar mensagem privada.\n')
 
-
-  # Solicita ao usuário inserir um nome de usuário
-  username = input('Usuário> ')
-  print('\nConectado')
-
-
-  # Cria duas threads para lidar com a recepção e envio de mensagens simultaneamente
-  thread1 = threading.Thread(target=receiveMessages, args=[client])
-  thread2 = threading.Thread(target=sendMessages, args=[client, username])
-
-
-  # Inicia as threads
-  thread1.start()
-  thread2.start()
-
+    # Inicia threads para receber e enviar mensagens
+    threading.Thread(target=receiveMessages, args=(client,)).start()
+    threading.Thread(target=sendMessages, args=(client, username)).start()
 
 def receiveMessages(client):
-  # Loop para receber mensagens do servidor
-  while True:
-      try:
-          # Recebe uma mensagem codificada em UTF-8 e a decodifica
-          msg = client.recv(2048).decode('utf-8')
-          # Exibe a mensagem recebida
-          print(msg+'\n')
-      except:
-          # Se houver um erro ao receber mensagens, exibe uma mensagem e encerra a conexão
-          print('\nNão foi possível permanecer conectado no servidor!\n')
-          print('Pressione <Enter> Para continuar...')
-          client.close()
-          break
-
+    while True:
+        try:
+            msg = client.recv(2048).decode('utf-8')
+            print(f"\n{msg}")
+        except:
+            print('\nConexão encerrada pelo servidor!')
+            client.close()
+            break
 
 def sendMessages(client, username):
-  # Loop para enviar mensagens para o servidor
-  while True:
-      try:
-          # Solicita ao usuário inserir uma mensagem
-          msg = input('\n')
-          # Envia a mensagem formatada com o nome de usuário ao servidor
-          client.send(f'<{username}> {msg}'.encode('utf-8'))
-      except:
-          # Se houver um erro ao enviar mensagens, encerra a thread
-          return
+    while True:
+        try:
+            msg = input()
+            if msg.startswith('/w '):
+                # Envia mensagem privada
+                client.send(msg.encode('utf-8'))
+            else:
+                # Envia mensagem normal (broadcast)
+                client.send(f"<{username}> {msg}".encode('utf-8'))
+        except:
+            print('Erro ao enviar mensagem.')
+            client.close()
+            break
 
-
-# Chama a função main para iniciar o cliente
-main()
+if __name__ == "__main__":
+    main()
